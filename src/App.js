2a940a4a72e3ed94d33a380c408236ed71948999
Shailesh Yogendra <shaileshyogendra1999@gmail.com>
2020-05-13 21:09:33 +0530

    added dark

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
 import image from './images/image.png';
+import { Paper, Switch } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 
 class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      data: {},
+      country: '',
+      darkMode:false
+    }
   }
-
+  
+  
+  
+  
   async componentDidMount() {
     const data = await fetchData();
 
@@ -24,16 +35,34 @@ class App extends React.Component {
     this.setState({ data, country: country });
   }
 
+  clickHandler =() => {
+    this.setState({
+      darkMode:!this.state.darkMode
+    })
+  }
+  
   render() {
+    
+//var darkMode=false;
+const theme = createMuiTheme({
+  palette: {
+    type: this.state.darkMode?'dark':'light',
+  },
+});
     const { data, country } = this.state;
 
     return (
+      <ThemeProvider theme={theme}>
+      <Paper square>
       <div className={styles.container}>
+      <Switch checked={this.state.darkMode} onChange={this.clickHandler}/>
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
       </div>
+      </Paper>
+      </ThemeProvider>
     );
   }
 }
